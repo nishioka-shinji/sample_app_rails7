d383ac3fbd9da617cd192da3f790c69e605d6157
nishioka-shinji <s61nov11.shinji.nishioka@gmail.com>
2022-06-04 18:10:09 +0900

    nginxとunicornの連携

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -25,8 +25,8 @@ working_directory File.expand_path('../../', __FILE__) # available in 0.94.0+
 
 # listen on both a Unix domain socket and a TCP port,
 # we use a shorter backlog for quicker failover when busy
-# listen "/path/to/.unicorn.sock", :backlog => 64
-listen 8080
+listen File.expand_path('../../tmp/sockets/.unicorn.sock', __FILE__), :backlog => 64
+# listen 8080, :tcp_nopush => true
 
 # nuke workers after 30 seconds instead of 60 seconds (the default)
 timeout 30
