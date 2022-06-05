8af0de4389bd7628aa6a8d804f3dfc7632b37dc9
nishioka-shinji <s61nov11.shinji.nishioka@gmail.com>
2022-06-05 15:29:59 +0900

    railsログを標準出力に

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,5 +68,9 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
+  # DNSリバインディング攻撃からの保護ホワイトリスト
   config.hosts << 'unicorn'
+
+  # ログを標準出力
+  config.logger = Logger.new(STDOUT)
 end
