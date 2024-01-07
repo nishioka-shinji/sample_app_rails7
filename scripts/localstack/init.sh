0c7079017fe68be6f9040a22fc32b19985fbeeee
nishioka-shinji <s61nov11.shinji.nishioka@gmail.com>
2024-01-08 07:55:12 +0900

    デッドレターキューの設定

--- scripts/localstack/init.sh
+++ scripts/localstack/init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# SQS + Lambda
+# SQS + Lambda(デッドレターキュー）
 awslocal sqs create-queue --queue-name sample_app_rails7_sqs_dlq
 awslocal lambda create-function \
     --function-name my-function \
@@ -13,4 +13,10 @@ awslocal lambda create-event-source-mapping \
     --event-source-arn arn:aws:sqs:us-east-1:000000000000:sample_app_rails7_sqs_dlq
 
 # SQS + ActiveJob
-awslocal sqs create-queue --queue-name sample_app_rails7_sqs
+awslocal sqs create-queue --queue-name sample_app_rails7_sqs \
+    --attributes '{"RedrivePolicy":"{\"maxReceiveCount\":\"3\", \"deadLetterTargetArn\":\"arn:aws:sqs:us-east-1:000000000000:sample_app_rails7_sqs_dlq\"}"}'
+
+# デッドレターキューとして使用できるソースキューを設定
+awslocal sqs set-queue-attributes \
+    --queue-url http://localhost:4566/000000000000/sample_app_rails7_sqs_dlq \
+    --attributes '{"RedriveAllowPolicy":"{\"redrivePermission\":\"byQueue\", \"sourceQueueArns\":\"arn:aws:sqs:us-east-1:000000000000:sample_app_rails7_sqs\"}"}'
